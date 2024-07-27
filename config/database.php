e8d8aa150d71439455631128c55aff5edac6be92
unknown <sperzvinas@gmail.com>
2024-07-27 12:33:28 -0500

    Added directories

---
+++ config/database.php
@@ -0,0 +1,19 @@
+<?php
+class Database {
+    private $host = "localhost";
+    private $db_name = "psuGangWeb";
+    private $username = "root";
+    private $password = "root";
+    public $connection ;
+    public function getConnection() {
+     $this->connection = null;
+    try {
+        $this->connection = new PDO("mysql:host=" . $this->host . ";dbname=" .
+        $this->db_name, $this->username, $this->password);
+        $this->connection->exec("set names utf8");
+    } catch(PDOException $exception) {
+        echo "connectionection error: " . $exception->getMessage();
+    }
+     return $this->connection;
+    }
+}
