29774ccd528ad3bbaf7f50614f86c1f613d6306e
ikabeee <carlglz30@gmail,com>
2024-07-27 21:50:01 -0500

    Archives for Carlos Modules

--- app/views/client/soporteTecnico/chatbotLiz.php
+++ app/views/client/soporteTecnico/chatbotLiz.php
@@ -1,25 +1,39 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PSU Gang | Inicio</title>
 </head>
+
 <body>
+
     <body class="wrapper">
         <div class="wrapper">
-            
             <?php include "navBar.php"; ?>
             <div class="content-wrapper">
                 <section class="content">
-                     <!-- Incluir contenido de los modulos -->
+                    <!-- Incluir contenido de los modulos -->
                     <div class="content-fluid">
+                        <div>
+                            <h1>Conversa con Liz!</h1>
+                        </div>
+                    </div>
+                    <div>
+                        <form>
+                                <input type="text" name="prompt" placeholder="¡Hola Liz! Tengo una duda">
+                            </div>
+                        </form>
                     </div>
                 </section>
             </div>
-            <?php include "footer.php"; ?>
+            <?php 
+            // include "footer.php"; 
+            ?>
         </div>
     </body>
-    
+
 </body>
+
 </html>
