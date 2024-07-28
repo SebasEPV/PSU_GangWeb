78ed62d68e27f7f9048af1a57aee880ad8e8af9f
ikabeee <carlglz30@gmail,com>
2024-07-28 14:17:10 -0500

    We deleted some modules

--- app/views/client/soporteTecnico/navBar.php
+++ app/views/client/soporteTecnico/navBar.php
@@ -5,22 +5,15 @@
   <!-- Required meta tags -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
-
-  <link rel="stylesheet" href="./../../../../public/assets/fonts/icomoon/style.css">
-
-  <link rel="stylesheet" href="./../../../../public/assets/css/owl.carousel.min.css">
-
+  <link rel="stylesheet" href="./../../../public/assets/fonts/icomoon/style.css">
+  <link rel="stylesheet" href="./../../../public/assets/css/owl.carousel.min.css">
   <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="./../../../../public/assets/css/bootstrap.min.css">
-
+  <link rel="stylesheet" href="./../../../public/assets/css/bootstrap.min.css">
   <!-- Style -->
-  <link rel="stylesheet" href="./../../../../public/assets/css/style.css">
-  <link href="./../../../public/assets/img/logos/capacitorLogo.png" rel="icon">
-
+  <link rel="stylesheet" href="./../../../public/assets/css/style.css">
+  <link href="./../../public/assets/img/logos/capacitorLogo.png" rel="icon">
   <title>PSU Gang Menu</title>
-
 </head>
 
 <body>
@@ -33,123 +26,120 @@
     </div>
     <div class="site-mobile-menu-body"></div>
   </div>
-
-
   <header class="site-navbar js-sticky-header site-navbar-target" role="banner">
-
     <div class="container">
       <div class="row align-items-center">
+
+
         <div class="col-6 col-xl-2">
-          <h1 class="mb-0 site-logo">
-            <a href="mainClient.php">
-              <img src="./../../../../public/assets/img/logo/capacitores/webp/capacitor5.webp" alt="capacitor.png" class="logo"></img>
-            </a>
-          </h1>
+          <a href="mainClient.php"><img class="mb-0 logo" src="./../../../public/assets/img/logo/capacitores/png/capacitor5.png" alt="capacitor"></img></a>
         </div>
+
+
+        <div class="col-12 col-md-10 d-none d-xl-block">
+          <nav class="site-navigation position-relative text-right" role="navigation">
+            <ul class="site-menu main-menu js-clone-nav mr-auto d-none d-lg-block">
+              <li class="has-children">
+                <a href="#about-section" class="nav-link">Nosotros</a>
+                <ul class="dropdown">
+                  <li><a href="#team-section" class="nav-link">FAQ</a></li>
+                  <li><a href="#pricing-section" class="nav-link">Nuestro equipo</a></li>
+                  <li><a href="#faq-section" class="nav-link">Colaboraciones</a></li>
+                  <li><a href="#faq-section" class="hidden" hidden>¡Conoce a Liz!</a></li>
+                  <li><a href="#faq-section" class="hidden" hidden>Donaciones</a></li>
+                </ul>
+              </li>
+              <li class="has-children">
+                <a href="#blog-section" class="nav-link" hidden>Guías</a>
+                <ul class="dropdown">
+                  <a href="./../client/guias/topology.php" class="nav-link" id="">Topologías</a>
+                  <a href="./../client/guias/pressReleases.php" class="nav-link" id="">Comunicados</a>
+                  <a href="./../client/guias/computers.php" class="nav-link" id="">Ensambles</a>
+                  <li class="has-children">
+                    <a href="" class="nav-link" id="">Undervolt</a>
+                    <ul class="dropdown">
+                      <li><a href="./../client/guias/UV/amd.php">AMD</a></li>
+                      <li><a href="./../client/guias/UV/nvidia.php">NVIDIA</a></li>
+                      <li></li>
+                    </ul>
+                  </li>
+                  <li class="has-children">
+                    <a href="">Tablas rendimiento</a>
+                    <ul class="dropdown">
+                      <li><a href="./../client/guias/tablasRendimiento/gpu.php">GPU</a></li>
+                      <li><a href="./../client/guias/tablasRendimiento/gpu.php">CPU</a></li>
+                    </ul>
+                    </ui>
+                  </li>
+                </ul>
+              </li>
+              <li class="has-children">
+                <a href="#blog-section" class="nav-link">Reseñas</a>
+                <ul class="dropdown">
+                  <a href="./../client/reseñas/psuList.php" class="nav-link" id="">Fuentes de poder</a>
+                  <a href="./../client/reseñas/motherboards.php" class="nav-link" id="" hidden>Tarjetas madre</a>
+                  <a href="./../client/reseñas/ssdList.php" class="nav-link" id="">Unidades de almacenamiento</a>
+                  <li class="has-children">
+                    <a href="" hidden>Perifericos</a>
+                    <ul class="dropdown">
+                      <li><a href="./../client/reseñas/perifericos/keyboards.php">Teclados</a></li>
+                      <li><a href="./../client/reseñas/perifericos/mouses.php">Ratones</a></li>
+                      <li><a href="./../client/reseñas/perifericos/headphones.php">Audífonos</a></li>
+                      <li><a href="./../client/reseñas/perifericos/microphones.php">Microfonos</a></li>
+                    </ul>
+                  </li>
+                </ul>
+              </li>
+              <li class="has-children">
+                <a href="#blog-section" class="nav-link" hidden>Tier Lists</a>
+                <ul class="dropdown">
+                  <a href="./../client/psuList.php" class="nav-link" id="">Fuentes de poder</a>
+                  <a href="./../client/tierLists/motherboards.php" class="nav-link" id="">Tarjetas madre</a>
+                  <a href="./../client/tierLists/monitors.php" class="nav-link" id="">Monitores</a>
+                  <a href="./../client/tierLists/ssd.php" class="nav-link" id="">Unidades de almacenamiento</a>
+                  <a href="./../client/tierLists/heatsinks.php" class="nav-link" id="">Disipadores</a>
+                  <li class="has-children">
+                    <a href="">Perifericos</a>
+                    <ul class="dropdown">
+                      <li><a href="./../client/tierLists/perifericos/keyboards.php">Teclados</a></li>
+                      <li><a href="./../client/tierLists/perifericos/mouses.php">Ratones</a></li>
+                      <li><a href="./../client/tierLists/perifericos/headphones.php">Audífonos</a></li>
+                      <li><a href="./../client/tierLists/perifericos/microphones.php">Microfonos</a></li>
+                    </ul>
+                  </li>
+                </ul>
+              </li>
+
+              <li class="has-children">
+                <a href="#blog-section" class="nav-link">Soporte técnico</a>
+                <ul class="dropdown">
+                  <li><a href="./../client/soporteTecnico/locations.php">Ubicaciones</a></li>
+                  <li><a href="./../client/soporteTecnico/conditionService.php">Condiciones de Servicios</a></li>
+                  <li><a href="./../client/soporteTecnico/chatbotLiz.php">Platica con Liz</a></li>
+                  <li><a href="./../client/soporteTecnico/wattageCalculator.php">Calculadora de wattage</a></li>
+                </ul>
+              </li>
+
+              <li class="social"><a href="https://www.facebook.com/groups/584894212470592/user/100088867867247/" class="nav-link"><span class="icon-facebook"></span></a></li>
+              <li class="social"><a href="https://www.instagram.com/psugangts/" class="nav-link"><span class="icon-instagram"></span></a></li>
+              <li class="social"><a href="https://www.youtube.com/@HardwareHubbyYGA" class="nav-link"><span class="icon-youtube"></span></a></li>
+              <li class="social"><a href="https://github.com/psugang" class="nav-link"><span class="icon-github"></span></a></li>
+            </ul>
+          </nav>
+        </div>
+
+
+        <div class="col-6 d-inline-block d-xl-none ml-md-0 py-3" style="position: relative; top: 3px;"><a href="#" class="site-menu-toggle js-menu-toggle float-right"><span class="icon-menu h3"></span></a></div>
+
       </div>
-
-      <div class="col-12 col-md-10 d-none d-xl-block">
-        <nav class="site-navigation position-relative text-right" role="navigation">
-
-          <ul class="site-menu main-menu js-clone-nav mr-auto d-none d-lg-block">
-            <li class="has-children">
-              <a href="#about-section" class="nav-link">Nosotros</a>
-              <ul class="dropdown">
-                <li><a href="#team-section" class="nav-link">FAQ</a></li>
-                <li><a href="#pricing-section" class="nav-link">Nuestro equipo</a></li>
-                <li><a href="#faq-section" class="nav-link">Colaboraciones</a></li>
-                <li><a href="#faq-section" class="nav-link">¡Conoce a Liz!</a></li>
-                <li><a href="#faq-section" class="nav-link">Donaciones</a></li>
-              </ul>
-            </li>
-            <li class="has-children">
-              <a href="#blog-section" class="nav-link">Guías</a>
-              <ul class="dropdown">
-                <a href="" class="nav-link" id="">Topologías</a>
-                <a href="" class="nav-link" id="">Comunicados</a>
-                <a href="" class="nav-link" id="">Ensambles</a>
-                <li class="has-children">
-                  <a href="" class="nav-link" id="">Undervolt</a>
-                  <ul class="dropdown">
-                    <li><a href="">AMD</a></li>
-                    <li><a href="">NVIDIA</a></li>
-                    <li></li>
-                  </ul>
-                </li>
-                <li class="has-children">
-                  <a href="">Tablas rendimiento</a>
-                  <ul class="dropdown">
-                    <li><a href="">GPU</a></li>
-                    <li><a href="">CPU</a></li>
-                  </ul>
-                  </ui>
-                </li>
-              </ul>
-            </li>
-            <li class="has-children">
-              <a href="#blog-section" class="nav-link">Reseñas</a>
-              <ul class="dropdown">
-                <a href="" class="nav-link" id="">Fuentes de poder</a>
-                <a href="" class="nav-link" id="">Tarjetas madre</a>
-                <a href="" class="nav-link" id="">Unidades de almacenamiento</a>
-                <li class="has-children">
-                  <a href="">Perifericos</a>
-                  <ul class="dropdown">
-                    <li><a href="">Teclados</a></li>
-                    <li><a href="">Ratones</a></li>
-                    <li><a href="">Audífonos</a></li>
-                    <li><a href="">Microfonos</a></li>
-                  </ul>
-                </li>
-              </ul>
-            </li>
-            <li class="has-children">
-              <a href="#blog-section" class="nav-link">Tier Lists</a>
-              <ul class="dropdown">
-                <a href="" class="nav-link" id="">Fuentes de poder</a>
-                <a href="" class="nav-link" id="">Tarjetas madre</a>
-                <a href="" class="nav-link" id="">Monitores</a>
-                <a href="" class="nav-link" id="">Unidades de almacenamiento</a>
-                <a href="" class="nav-link" id="">Disipadores</a>
-                <li class="has-children">
-                  <a href="">Perifericos</a>
-                  <ul class="dropdown">
-                    <li><a href="">Teclados</a></li>
-                    <li><a href="">Ratones</a></li>
-                    <li><a href="">Audífonos</a></li>
-                    <li><a href="">Microfonos</a></li>
-                  </ul>
-                </li>
-              </ul>
-            </li>
-
-            <li class="has-children">
-              <a href="#blog-section" class="nav-link">Soporte técnico</a>
-              <ul class="dropdown">
-                <li><a href="soporteTecnico/locations.php" >Ubicaciones</a></li>
-                <li><a href="client/soporteTecnico/conditionService.php" >Condiciones de Servicios</a></li>
-                <li><a href="client/soporteTecnico/chatbotLiz.php">Platica con Liz</a></li>
-                <li><a href="client/soporteTecnico/wattageCalculator.php">Calculadora de wattage</a></li>
-              </ul>
-            </li>
-
-            <li class="social"><a href="https://www.facebook.com/groups/584894212470592/user/100088867867247/" class="nav-link"><span class="icon-facebook"></span></a></li>
-            <li class="social"><a href="https://www.instagram.com/psugangts/" class="nav-link"><span class="icon-instagram"></span></a></li>
-            <li class="social"><a href="https://www.youtube.com/@HardwareHubbyYGA" class="nav-link"><span class="icon-youtube"></span></a></li>
-            <li class="social"><a href="https://github.com/psugang" class="nav-link"><span class="icon-github"></span></a></li>
-          </ul>
-        </nav>
-      </div>
-      <div class="col-6 d-inline-block d-xl-none ml-md-0 py-3" style="position: relative; top: 3px;"><a href="#" class="site-menu-toggle js-menu-toggle float-right"><span class="icon-menu h3"></span></a></div>
-    </div>
     </div>
   </header>
-  <div class="hero"></div>
-  <script src="./../../../../public/assets/js/jquery-3.3.1.min.js"></script>
-  <script src="./../../../../public/assets/js/popper.min.js"></script>
-  <script src="./../../../../public/assets/js/bootstrap.min.js"></script>
-  <script src="./../../../../public/assets/js/jquery.sticky.js"></script>
-  <script src="./../../../../public/assets/js/main.js"></script>
+  <div class="hero"> </div>
+  <script src="./../../../public/assets/js/jquery-3.3.1.min.js"></script>
+  <script src="./../../../public/assets/js/popper.min.js"></script>
+  <script src="./../../../public/assets/js/bootstrap.min.js"></script>
+  <script src="./../../../public/assets/js/jquery.sticky.js"></script>
+  <script src="./../../../public/assets/js/main.js"></script>
 </body>
 
 </html>
